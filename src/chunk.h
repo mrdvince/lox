2409c273b1e704c0e3347d4bf3dd6b024497f518
Vince <vincivenv@gmail.com>
2022-07-05 21:30:41 +0200

    * constants valuearray

--- src/chunk.h
+++ src/chunk.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "common.h"
+#include "value.h"
 
 typedef enum {
     OP_RETURN,
@@ -9,8 +10,10 @@ typedef struct {
     int capacity;
     int count;
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 void freeChunk(Chunk* chunk);
---
+++ src/value.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "common.h"
+
+typedef double Value;
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
