e42ca55c5c6ea3c6bf4c693142c30bd4bb0b84b3
Vince <vincivenv@gmail.com>
2022-07-07 16:11:09 +0200

    constant instruction

--- src/value.h
+++ src/value.h
@@ -10,3 +10,4 @@ typedef struct {
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
+void printValue(Value value);
